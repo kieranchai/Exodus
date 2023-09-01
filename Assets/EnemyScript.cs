cd10d9b0d8b13403b70be608a6796d5a9b9c17bd
JoelYeet <joelyeeyongyi99@gmail.com>
2023-09-01 23:23:21 +0800

    replace sprites

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -7,12 +7,15 @@ public class EnemyScript : MonoBehaviour
 {
 
     NavMeshAgent agent;
+    public Animator anim;
+    public Rigidbody2D rb;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        anim.SetBool("isWalking", true);
     }
     void Update()
     {
